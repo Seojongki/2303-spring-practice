37029231116eccdf0fa1231912c8dff6867844dd
Seojongki <jongkis@gmail.com>
2023-07-06 11:28:39 +0900

    ..

---
+++ src/main/java/com/tj/edu/practice2/ioc/Base64Encoder.java
@@ -0,0 +1,21 @@
+package com.tj.edu.practice2.ioc;
+
+import java.util.Base64;
+import java.util.Base64.Encoder;
+
+public class Base64Encoder implements IEncoder{
+
+        @Override
+        public String encodeString(String message) {
+            // Base64 인코딩 ///////////////////////////////////////////////////
+            return Base64.getEncoder().encodeToString(message.getBytes());
+        }
+
+        public byte[] encodeByte(String message) {
+            // Base64 인코딩 ///////////////////////////////////////////////////
+            Encoder encoder = Base64.getEncoder();
+            byte[] encodedBytes = encoder.encode(message.getBytes());
+
+        return encodedBytes;
+    }
+}
---
+++ src/main/java/com/tj/edu/practice2/spring_ioc/UrlEncoder.java
@@ -0,0 +1,31 @@
+package com.tj.edu.practice2.spring_ioc;
+
+
+import org.springframework.stereotype.Component;
+
+import java.net.URLEncoder;
+
+
+@Component
+public class UrlEncoder implements IEncoder {
+
+    @Override
+    public String encodeString(String message) {
+        try {
+            return URLEncoder.encode(message, "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public byte[] encodeByte(String message) {
+        try {
+            return URLEncoder.encode(message, "UTF-8").getBytes();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
